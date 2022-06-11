d05fcd7a1e84e4eef54423302a6d054d000726c8
Alexandr <yourappsdeveloper@gmail.com>
2022-06-11 23:28:15 +0300

    Bugs fixing

--- Source/Views/FooterView.swift
+++ Source/Views/FooterView.swift
@@ -4,6 +4,12 @@ open class PlaybackSlider: UISlider {
     open override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         return true
     }
+    
+    open override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+       var bounds: CGRect = self.bounds
+       bounds = bounds.insetBy(dx: -10, dy: -15)
+       return bounds.contains(point)
+    }
 }
 
 public protocol FooterViewDelegate: AnyObject {
