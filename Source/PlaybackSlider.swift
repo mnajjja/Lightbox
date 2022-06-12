4f3ff9891e4b9b0861fc74eb49816b2cccd458d6
Alexandr <yourappsdeveloper@gmail.com>
2022-06-12 16:19:25 +0300

    Move player thumb with touch on track progress bar

--- Source/PlaybackSlider.swift
+++ Source/PlaybackSlider.swift
@@ -9,6 +9,22 @@ import UIKit
 
 open class PlaybackSlider: UISlider {
     open override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let widthOfThumb: Float = Float(self.thumbImage(for: .focused)?.size.width ?? 20)
+        let pointTappedX: Float = Float(touch.location(in: self).x)
+        let widthOfSlider: Float = Float(self.frame.size.width)
+        var newValue = pointTappedX / widthOfSlider * self.maximumValue
+        
+        /// Move Forward
+        if newValue > self.value {
+            newValue += (widthOfThumb / 2) / widthOfSlider * self.maximumValue
+        }
+        /// Move Back
+        else if newValue < self.value{
+            newValue -= (widthOfThumb / 2) / widthOfSlider * self.maximumValue
+        }
+        
+        self.setValue(newValue, animated: true)
+        
         return true
     }
     
