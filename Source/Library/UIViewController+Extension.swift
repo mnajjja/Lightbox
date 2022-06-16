4b9d1621fd64301e5846d9b9568fd48131ac52ac
Aleksandr Volodchenko <yourappsdeveloper@gmail.com>
2022-06-16 19:48:54 +0300

    Update Alert Texts

--- Source/Library/UIViewController+Extension.swift
+++ Source/Library/UIViewController+Extension.swift
@@ -11,17 +11,17 @@ import UIKit
 extension UIViewController {
     
     func goToSettings() {
-        let alert = UIAlertController(title: "Allow access to your photos",
-                                      message: "This lets you save photos and videos. Go to your settings and tap \"Photos\".", preferredStyle: UIAlertController.Style.alert)
+        let alert = UIAlertController(title: "Photo library access denied",
+                                      message: "Go to Settings to update permissions", preferredStyle: UIAlertController.Style.alert)
         
-        alert.addAction(UIAlertAction(title: "Settings", style: UIAlertAction.Style.default, handler: { [weak self] _ in
+        alert.addAction(UIAlertAction(title: "Go to Settings", style: UIAlertAction.Style.default, handler: { [weak self] _ in
             guard self != nil else { return }
             if let appSettings = URL(string: UIApplication.openSettingsURLString), UIApplication.shared.canOpenURL(appSettings) {
                 UIApplication.shared.open(appSettings)
             }
             
         }))
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: nil))
         
         self.present(alert, animated: true, completion: nil)
     }
