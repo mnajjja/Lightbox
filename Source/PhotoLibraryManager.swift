dd9efbc190d54b986aae433b05fa5df3f1f53718
Patryk Średziński <sredzinskipatryk@gmail.com>
2022-12-27 13:32:35 +0100

    Use local path to store media in gallery instead of refetching

--- Source/PhotoLibraryManager.swift
+++ Source/PhotoLibraryManager.swift
@@ -12,18 +12,40 @@ import Foundation
 
 enum PhotoLibraryManager {
     
-    static func saveVideo(from url: URL, completionHandler: ((Bool, Error?) -> Void)? = nil) {
+    typealias Completion = (Bool, Error?) -> Void
+    
+    static func saveVideo(from url: URL, completionHandler: Completion? = nil) {
+        if url.isFileURL {
+            storeVideoAsset(url, completionHandler: completionHandler)
+        } else {
+            fetchAndStoreVideo(from: url, completionHandler: completionHandler)
+        }
+    }
+    
+    static func saveImage(from url: URL, completionHandler: Completion? = nil) {
+        if url.isFileURL {
+            storePhotoAsset(url, completionHandler: completionHandler)
+        } else {
+            fetchAndStoreImage(from: url, completionHandler: completionHandler)
+        }
+    }
+    
+    static func saveImage(_ image: UIImage, completionHandler: Completion? = nil) {
+        storeImage(image, completionHandler: completionHandler)
+    }
+}
+
+private extension PhotoLibraryManager {
+    static func fetchAndStoreVideo(from url: URL, completionHandler: Completion?) {
         let fileName = url.lastPathComponent
         DispatchQueue.global(qos: .background).async {
             if let urlData = NSData(contentsOf: url) {
-                let galleryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0];
-                let filePath="\(galleryPath)/\(fileName)"
+                let galleryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+                let filePath = "\(galleryPath)/\(fileName)"
                 DispatchQueue.main.async {
                     urlData.write(toFile: filePath, atomically: true)
-                    PHPhotoLibrary.shared().performChanges({
-                        PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL:
-                                                                                URL(fileURLWithPath: filePath))
-                    }, completionHandler: completionHandler)
+                    let url = URL(fileURLWithPath: filePath)
+                    storeVideoAsset(url, completionHandler: completionHandler)
                 }
             } else {
                 completionHandler?(false, nil)
@@ -31,18 +53,16 @@ enum PhotoLibraryManager {
         }
     }
     
-    static func saveImage(from url: URL, completionHandler: ((Bool, Error?) -> Void)? = nil) {
+    static func fetchAndStoreImage(from url: URL, completionHandler: Completion?) {
         DispatchQueue.global(qos: .background).async {
             let fileName = url.lastPathComponent
             if let urlData = NSData(contentsOf: url) {
-                let galleryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0];
-                let filePath="\(galleryPath)/\(fileName)"
+                let galleryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+                let filePath = "\(galleryPath)/\(fileName)"
                 DispatchQueue.main.async {
                     urlData.write(toFile: filePath, atomically: true)
-                    PHPhotoLibrary.shared().performChanges({
-                        PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL:
-                                                                                URL(fileURLWithPath: filePath))
-                    }, completionHandler: completionHandler)
+                    let url = URL(fileURLWithPath: filePath)
+                    storePhotoAsset(url, completionHandler: completionHandler)
                 }
             } else {
                 completionHandler?(false, nil)
@@ -50,22 +70,29 @@ enum PhotoLibraryManager {
         }
     }
     
-    static func saveImage(_ image: UIImage, completionHandler: ((Bool, Error?) -> Void)? = nil) {
-        DispatchQueue.global(qos: .background).async {
-            if let imageData = image.pngData() {
-                let galleryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0];
-                let filePath="\(galleryPath)/\(Date().toString()).png"
-                
-                DispatchQueue.main.async {
-                    try? imageData.write(to: URL(fileURLWithPath: filePath))
-                    PHPhotoLibrary.shared().performChanges({
-                        PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL:
-                                                                                URL(fileURLWithPath: filePath))
-                    }, completionHandler: completionHandler)
-                }
-            } else {
-                completionHandler?(false, nil)
-            }
+    static func storeImage(_ image: UIImage, completionHandler: Completion?) {
+        DispatchQueue.main.async {
+            storeImageAsset(image, completionHandler: completionHandler)
         }
+    }
+}
+
+private extension PhotoLibraryManager {
+    private static func storeVideoAsset(_ localUrl: URL, completionHandler: Completion?) {
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: localUrl)
+        }, completionHandler: completionHandler)
+    }
+    
+    private static func storePhotoAsset(_ localUrl: URL, completionHandler: Completion?) {
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: localUrl)
+        }, completionHandler: completionHandler)
+    }
+    
+    private static func storeImageAsset(_ image: UIImage, completionHandler: Completion?) {
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAsset(from: image)
+        }, completionHandler: completionHandler)
     }
 }
